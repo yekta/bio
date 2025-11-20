0fd5daadc5ac81e50055972f1be0a822f0610abc
yekta <ngyekta@gmail.com>
2025-11-20 09:51:38 +0300

    Style tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,24 @@ export default function Home() {
     <div className="text-foreground relative flex min-h-svh w-full items-center justify-center overflow-hidden font-sans">
       <main className="relative flex w-full max-w-3xl flex-col items-center justify-center px-2 pt-8 pb-[calc(1rem+10vh)]">
         <div className="relative flex max-w-full flex-col items-center">
-          <div className="relative w-full flex-col items-center gap-1.5">
-            <div className="flex w-full items-center justify-center gap-1.5">
+          {/* Avatar section */}
+          <div className="relative flex w-full max-w-md flex-col items-center gap-0.5 px-4">
+            <div className="flex w-full items-center justify-start gap-1.5">
               <Image
-                className="bg-border w-7 rounded-full border"
+                className="w-6.5 rounded-full border"
                 src="/avatar.png"
                 alt="Avatar"
                 width={486}
                 height={486}
-                sizes="28px"
+                sizes="26px"
               />
               <p className="min-w-0 shrink overflow-hidden font-mono text-lg font-extrabold tracking-tight overflow-ellipsis">
                 {displayName}
               </p>
             </div>
-            <p className="text-muted-foreground text w-full text-center text-sm">{description}</p>
+            <p className="text-muted-foreground text w-full px-0.5 text-sm">{description}</p>
           </div>
-          <ScCards className="relative pt-5" />
+          <ScCards className="relative pt-4" />
         </div>
       </main>
     </div>
