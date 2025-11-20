5404ddddfdb40292a14837a9419f566f27e323f3
yekta <ngyekta@gmail.com>
2025-11-20 07:01:00 +0300

    ScCard improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import ScCard from "@/components/sc-card";
 import ScCards from "@/components/sc-cards";
-import { description, displayName, scLinks } from "@/lib/constants";
+import { description, displayName } from "@/lib/constants";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-background text-foreground flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center px-2 pt-8 pb-[calc(2rem+10vh)]">
-        {/* Avatar section */}
+      <main className="relative flex min-h-screen w-full max-w-3xl flex-col items-center justify-center px-2 pt-8 pb-[calc(2rem+10vh)]">
         <div className="flex w-full flex-col items-center gap-1.5">
           <div className="flex w-full items-center justify-center gap-1.5">
             <Image
@@ -18,13 +16,12 @@ export default function Home() {
               height={486}
               sizes="28px"
             />
-            <p className="min-w-0 shrink overflow-hidden font-mono text-lg leading-tight font-extrabold overflow-ellipsis">
+            <p className="min-w-0 shrink overflow-hidden font-mono text-lg font-extrabold tracking-tight overflow-ellipsis">
               {displayName}
             </p>
           </div>
           <p className="text-muted-foreground text w-full text-center text-sm">{description}</p>
         </div>
-        {/* Cards */}
         <ScCards className="pt-5" />
       </main>
     </div>
--- components/sc-card.tsx
+++ components/sc-card.tsx
@@ -15,13 +15,15 @@ export default function ScCard({ data: { label, handle, href, Icon }, isHighligh
       className="group flex w-full max-w-lg items-center justify-start rounded-xl p-1"
       data-highlighted={isHighlighted ? true : undefined}
     >
-      <div className="group-hover:bg-border group-active:bg-border relative flex w-full items-center gap-2 overflow-hidden rounded-lg border px-3 py-3">
-        <div className="bg-primary/50 pointer-events-none absolute top-0 left-0 h-full w-4 opacity-0 blur-xl group-data-highlighted:opacity-100" />
-        <div className="from-primary/15 via-primary/0 to-primary/0 pointer-events-none absolute top-0 left-0 h-full w-full bg-linear-to-r opacity-0 group-data-highlighted:opacity-100" />
+      <div className="group-hover:bg-border group-active:bg-border relative flex w-full items-center gap-2 overflow-hidden rounded-lg px-3 py-3">
+        <div className="group-data-highlighted:from-primary/30 from-border via-border to-border pointer-events-none absolute top-0 left-0 h-full w-full rounded-lg bg-linear-to-r via-20% p-px">
+          <div className="group-hover:bg-border group-active:bg-border bg-background h-full w-full rounded-[calc(0.5rem-1px)]" />
+        </div>
+        <div className="from-primary/10 via-primary/0 to-primary/0 pointer-events-none absolute top-0 left-0 h-full w-full bg-linear-to-r via-40% opacity-0 group-data-highlighted:opacity-100" />
         <Icon className="relative size-6" />
-        <p className="relative w-full overflow-hidden leading-tight font-bold">
+        <p className="relative w-full overflow-hidden leading-tight font-bold tracking-tight">
           <span className="pr-[0.5ch]">{label}</span>
-          <span className="text-muted-foreground bg-muted-foreground/12 rounded-sm px-1.25 py-0.75 font-mono text-sm font-semibold">
+          <span className="text-muted-foreground bg-muted-foreground/12 rounded-sm px-1.25 py-0.75 font-mono text-xs font-semibold">
             @{handle}
           </span>
         </p>
