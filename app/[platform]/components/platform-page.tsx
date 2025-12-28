7304d242f8cc558e2f8ceb957810cf0587cc2f2c
yekta <ngyekta@gmail.com>
2025-12-29 02:07:54 +0300

    Redirect improvements

--- app/[platform]/components/platform-page.tsx
+++ app/[platform]/components/platform-page.tsx
@@ -3,21 +3,37 @@
 import ScIcon from "@/components/icons/sc/sc-icon";
 import { Button } from "@/components/ui/button";
 import { scLinks, TScEnum } from "@/lib/constants";
-import { LoaderIcon } from "lucide-react";
+import { CheckCircleIcon, LoaderIcon } from "lucide-react";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type TProps = {
   platform: TScEnum;
 };
 
 export default function PlatformPage({ platform }: TProps) {
+  const [isRedirected, setIsRedirected] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
   const platformObject = scLinks.find((link) => link.id === platform);
 
   useEffect(() => {
     if (platformObject) {
       window.location.href = platformObject.href;
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
+      }
+      timeoutRef.current = setTimeout(() => {
+        setIsRedirected(true);
+      }, 2000);
     }
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
+      }
+    };
   }, [platformObject]);
 
   if (!platformObject) {
@@ -36,10 +52,14 @@ export default function PlatformPage({ platform }: TProps) {
   }
 
   return (
-    <div className="my-auto flex w-full flex-col items-center gap-2 pb-[4vh]">
-      <LoaderIcon className="text-muted-foreground size-8 animate-spin" />
-      <h1 className="text-muted-foreground w-full text-center text-xl leading-relaxed font-medium">
-        Redirecting to
+    <div className="my-auto flex w-full flex-col items-center gap-1 pb-[4vh]">
+      {!isRedirected ? (
+        <LoaderIcon className="text-muted-foreground size-10 animate-spin" />
+      ) : (
+        <CheckCircleIcon className="text-muted-foreground size-10" />
+      )}
+      <h1 className="text-muted-foreground w-full text-center text-lg leading-relaxed font-medium">
+        {!isRedirected ? "Redirecting to" : "Redirected to"}
         <br />
         <span className="text-foreground text-3xl font-bold">
           <ScIcon platform={platform} className="mr-[0.25ch] mb-1 inline-block size-10" />
